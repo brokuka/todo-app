8eeddc2fbe435e0cea91078db6447d3d2ae9ed6d
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2023-01-30 14:28:43 +0400

    fix: theme in localStorage

--- src/hooks/useThemeSwitch.ts
+++ src/hooks/useThemeSwitch.ts
@@ -4,16 +4,21 @@ type Theme = "dark" | "light" | "system";
 
 const useThemeSwitch = () => {
   const themeStorage = window.localStorage.getItem("theme") as Theme;
-  const [theme, setTheme] = useState(themeStorage);
+
   const htmlElement = document.documentElement;
   const themeDarkMatch = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
+  const [theme, setTheme] = useState(
+    !themeStorage && !themeDarkMatch ? "light" : themeStorage
+  );
 
   const onWindowMatch = () => {
     if (themeStorage === "dark" || (!themeStorage && themeDarkMatch)) {
       htmlElement.classList.add("dark");
-    } else htmlElement.classList.remove("dark");
+    } else {
+      htmlElement.classList.remove("dark");
+    }
   };
   onWindowMatch();
 
@@ -28,9 +33,7 @@ const useThemeSwitch = () => {
         localStorage.setItem("theme", theme);
         break;
       default:
-        if (!themeDarkMatch && themeStorage) return;
-        localStorage.removeItem("theme");
-        break;
+        return;
     }
   }, [theme]);
 
